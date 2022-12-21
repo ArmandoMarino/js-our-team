6bbbe8231cf5e3dd988090c313e7a10e2f5bd78e
ArmandoMarino <scrivania.lavoro@gmail.com>
2022-12-21 15:49:33 +0100

    created list obj checked

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
 console.log("JS OK");
+
+
+/* DOM ELEMENT */
+
+
+const ourTeam = [
+{
+    //WAYNE 0
+    name : 'Wayne',
+    lastName : 'Barnett',
+    role : 'Founder & CEO',
+    imageLink : 'img/wayne-barnett-founder-ceo.jpg' 
+},
+{
+    name : 'Angela',
+    lastName : 'Caroll',
+    role : 'Chied Editor',
+    imageLink : 'angela-caroll-chief-editor.jpg' 
+},
+{
+    name : 'Walter',
+    lastName : 'Gordon',
+    role : 'Office Manager',
+    imageLink : 'walter-gordon-office-manager.jpg' 
+},
+{
+    name : 'Angela',
+    lastName : 'Lopez',
+    role : 'Social Media Manager',
+    imageLink : 'angela-lopez-social-media-manager.jpg' 
+},
+{
+    name : 'Scott',
+    lastName : 'Estrada',
+    role : 'Developer',
+    imageLink : 'scott-estrada-developer.jpg' 
+},
+{
+    name : 'Barbara',
+    lastName : 'Ramos',
+    role : 'Graphic Designer',
+    imageLink : 'barbara-ramos-graphic-designer.jpg' 
+},
+
+
+]
+// CHECK
+console.log(ourTeam[4].imageLink);
